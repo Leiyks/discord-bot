3b82ec253df3fa060af4c7974073f14b73786241
Alexandre Rulleau <rulleaualexandre@gmail.com>
2023-06-11 01:26:09 +0200

    feat(extensions: help): add link button and commands comment
    Signed-off-by: Alexandre Rulleau <rulleaualexandre@gmail.com>

--- discord_bot/extensions/gambling.py
+++ discord_bot/extensions/gambling.py
@@ -6,13 +6,13 @@ from discord_bot.main import Client
 
 class Gambling(Cog):
     """
-    This is the module doc
+    Commands collection of gambling commands.
     """
 
     @hybrid_command()  # type: ignore
     async def coinflip(self, context: Context):
         """
-        Coinflip help message
+        Flip a coin and tell if it tails or heads.
         """
         choice = random.choice(["head", "tail"])
         await context.reply(f"```It's {choice} !```")
@@ -20,7 +20,7 @@ class Gambling(Cog):
     @hybrid_command()  # type: ignore
     async def random(self, context: Context, min: int, max: int):
         """
-        Random help message
+        Give a random number between the two numbers given as parameters.
         """
         if min >= max:
             await context.reply("```First number must be lower that the second !```")
--- discord_bot/extensions/help.py
+++ discord_bot/extensions/help.py
@@ -6,8 +6,8 @@ from discord.ext.commands import Context, hybrid_command
 
 from importlib import metadata
 
-from discord import SelectOption, Interaction
-from discord.ui import View, select, Select
+from discord import SelectOption, Interaction, ButtonStyle
+from discord.ui import View, select, Select, Button
 from discord_bot.utils.communication import send
 from importlib import metadata
 
@@ -16,9 +16,54 @@ from discord_bot.main import client, Client
 MODULE_EMOJIS: Dict[str, str] = {"Gambling": "🎲", "Help": "🫴"}
 
 
+class HelpView(View):
+    """
+    Handle the interactivity of the help command.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_item(
+            Button(
+                style=ButtonStyle.link,
+                label="Github",
+                url="https://github.com/Leiyks/discord-bot",
+                emoji="<:github:1117230368215543828>",
+            )
+        )
+
+    @select(
+        placeholder="Pick a module !",
+        options=[
+            SelectOption(label=cog, emoji=MODULE_EMOJIS[cog], description=client.cogs[cog].__doc__)
+            for cog in list(client.cogs.keys())
+        ],
+    )  # type: ignore
+    async def help_module(self, interaction: Interaction, select: Select):
+        """Display help message for a specific module of the bot."""
+        await interaction.response.defer()
+
+        value: str = select.values[0]
+
+        embed: Embed = discord.Embed(
+            title=f"{MODULE_EMOJIS[value]} {value} Commands",
+            color=discord.Color.teal(),
+            description="\n".join(
+                [f"`{command.name}`: {command.help}" for command in client.cogs[value].get_commands()]
+            ),
+        )
+
+        await interaction.followup.send(embed=embed, ephemeral=True)
+
+
 class Help(commands.Cog):
+    """
+    Display custom interactive help message.
+    """
+
     @hybrid_command()  # type: ignore
     async def help(self, context: Context):
+        """Display main help message that can be interacted with."""
         version: str = metadata.version("discord-bot")
         prefix: str = str(client.command_prefix)
 
@@ -38,7 +83,7 @@ class Help(commands.Cog):
                 name="About",
                 value=(
                     "This bot is developed and maintained by Leiyks, based on the `discord.py` library.\n"
-                    "Please visit https://github.com/Leiyks/discord-bot to submit ideas or bugs."
+                    "Please visit the `Github project page` below to submit ideas or bugs."
                 ),
             )
             .set_footer(text=f"Bot running version: {version}")
@@ -46,30 +91,6 @@ class Help(commands.Cog):
 
         view = HelpView()
         await send(context, embed=embed, view=view)
-
-
-class HelpView(View):
-    @select(
-        placeholder="Pick a module !",
-        options=[
-            SelectOption(label=cog, emoji=MODULE_EMOJIS[cog], description=client.cogs[cog].__doc__)
-            for cog in list(client.cogs.keys())
-        ],
-    )  # type: ignore
-    async def help_module(self, interaction: Interaction, select: Select):
-        await interaction.response.defer(ephemeral=True)
-
-        value: str = select.values[0]
-
-        embed: Embed = discord.Embed(
-            title=f"{MODULE_EMOJIS[value]} {value} Commands",
-            color=discord.Color.teal(),
-            description="\n".join(
-                [f"`{command.name}`: {command.help}" for command in client.cogs[value].get_commands()]
-            ),
-        )
-
-        await interaction.followup.send(embed=embed)
 
 
 async def setup(client: Client):
